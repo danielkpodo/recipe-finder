7e910981a95714338821129082fac23b67a6ba2e
naphtha <naphthanewman@gmail.com>
2019-11-27 00:25:23 +0000

    removed social share btns

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -4,6 +4,7 @@ import { Link, withRouter } from "react-router-dom";
 class RecipeDetails extends Component {
   render() {
     const recipeId = this.props.match.params.label;
+    console.log(recipeId);
     const recipeInfo = this.props.recipes.filter(
       food => food.recipe.label.replace(/ /g, "-") === recipeId
     )[0];
@@ -12,7 +13,9 @@ class RecipeDetails extends Component {
       calories,
       image,
       healthLabels,
-      ingredientLines
+      ingredientLines,
+      source,
+      url
     } = recipeInfo.recipe;
     return (
       <div className="recipe-details">
@@ -20,23 +23,24 @@ class RecipeDetails extends Component {
           <div className="col s12">
             <Link
               to="/"
-              className="btn btn-small waves-effect waves-light lightred"
+              className="btn btn-small waves-effect waves-light back"
             >
               <i className="material-icons left">arrow_back</i> Go Back
             </Link>
           </div>
         </div>
         <div className="row">
-          <div className="col s12 m6 xl6">
+          <div className="col s12 m12 l6 xl6">
             <img src={image} alt={label} className="z-depth-3" />
           </div>
-          <div className="col s12 m6 xl6">
+          <div className="col s12 m12 l6 xl6">
             <h3>{label}</h3> <hr className="style-seven" />
             <h4>Health Labels</h4>
             <div className="labels">
               {healthLabels.map((item, index) => {
                 return (
                   <a
+                    href="#!"
                     key={index}
                     className="waves-effect waves-light btn  btn-small"
                   >
@@ -58,6 +62,22 @@ class RecipeDetails extends Component {
                 ))}
               </ul>
             </div>
+            <div className="source">
+              <h4>Recipe Owner</h4>
+              <p>
+                Visit
+                <a
+                  href={url}
+                  title={`Visit ${label} recipe cooking directions`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {source}
+                </a>
+                for step-by-step directions on cooking
+              </p>
+            </div>
+            <h4>Share to Family & Friends</h4>
           </div>
         </div>
       </div>
