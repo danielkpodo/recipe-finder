7e28df29fd08ae2d452931e562d297ce201fc23f
naphtha <naphthanewman@gmail.com>
2019-11-27 01:05:53 +0000

    refactored receipeDetails component

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -6,7 +6,13 @@ const RecipeCard = props => {
     <div className="col s12 m6 l4 xl3">
       <div className="card card-large">
         <div className="card-image">
-          <img src={image} alt={label} data-caption="Jollof Rice and Agushi" />
+          <Link to={`/recipe/${label.replace(/ /g, "-")}`} title={label}>
+            <img
+              src={image}
+              alt={label}
+              data-caption="Jollof Rice and Agushi"
+            />
+          </Link>
           <Link
             className="btn-floating btn-small halfway-fab waves-effect waves-light red"
             to={`/recipe/${label.replace(/ /g, "-")}`}
